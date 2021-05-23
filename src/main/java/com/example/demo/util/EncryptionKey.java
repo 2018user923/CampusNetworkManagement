e1e4909bc9cd4bea56dd5101b43e66c9bb9746d9
TuQi <247702560@qq.com>
2021-05-23 18:17:35 +0800

    index 添加邮箱发送按钮

--- src/main/java/com/example/demo/util/EncryptionKey.java
+++ src/main/java/com/example/demo/util/EncryptionKey.java
@@ -7,6 +7,9 @@ public class EncryptionKey {
     //网络流量数据
     public static String netData = "NET_DATA";
 
+    //注册账户发送邮箱验证码
+    public static String registerEmail = "REGISTER_EMAIL";
 
-
+    //邮箱登录账户验证码
+    public static String loginEmail = "LOGIN_EMAIL";
 }
